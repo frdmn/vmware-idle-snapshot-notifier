a88c0e9bb24cadc45717a12988047d88d49b428d
Jonas Friedmann <j@frd.mn>
2019-06-05 22:56:30 +0200

    Add argument to set path for configuration file

--- snapshots.py
+++ snapshots.py
@@ -20,6 +20,7 @@ from pyVim.connect import Disconnect, SmartConnect, GetSi
 # Create argument parser
 parser = argparse.ArgumentParser(description='Report idle VMware snapshots')
 parser.add_argument('--min-age-in-days', type=int, help='The minimum age in days of snapshots to report')
+parser.add_argument('--config', help='Path to configuration file')
 parser.add_argument('--debug', action='store_true', help='Enable debug mode (optional)')
 args = parser.parse_args()
 
@@ -30,11 +31,19 @@ if not args.min_age_in_days:
 
 # Load config file
 configurationFile = './config.json'
+
+if args.config:
+    if os.path.isfile(args.config):
+        configurationFile = args.config
+    else:
+        print("Couldn't read config file \"%s\"" % args.config)
+        sys.exit(1)
+
 if os.path.isfile(configurationFile):
     with open(configurationFile, 'r') as f:
         config = json.load(f)
 else:
-    print("Couldn't read config file \"config.json\"")
+    print("Couldn't read config file \"%s\"" % configurationFile)
     sys.exit(1)
 
 # Store todays date
