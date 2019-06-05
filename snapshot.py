d11a47873966a743463fcf287ace03e912aff20f
Jonas Friedmann <j@frd.mn>
2019-06-05 20:18:38 +0200

    English comments instead of german ones

--- snapshot.py
+++ snapshot.py
@@ -15,7 +15,7 @@ from pyVim.task import WaitForTask
 from pyVim import connect
 from pyVim.connect import Disconnect, SmartConnect, GetSi
 
-# Konfigurationsdatei laden
+# Load config file
 configurationFile = './config.json'
 if os.path.isfile(configurationFile):
     with open(configurationFile, 'r') as f:
@@ -24,11 +24,12 @@ else:
     print("Couldn't read config file \"config.json\"")
     sys.exit(1)
 
+# Function to conditionally print debug message
 def debug_print(msg):
     if config['debug']:
         print(msg)
 
-# Funktion um auf allgemeine VMware API Objekte zuzugreifen
+# Function to access API objects in general
 def get_obj(content, vimtype, name):
     obj = None
     container = content.viewManager.CreateContainerView(
@@ -39,46 +40,46 @@ def get_obj(content, vimtype, name):
             break
     return obj
 
-# (Recursive) Funktion um alle aktuellen Snapshots aufzulisten
+# (Recursive) function to list all snapshots
 def list_snapshots_recursively(vm_name, snapshots):
     snapshot_data = []
     snap_text = ""
     for snapshot in snapshots:
-        snap_text = "VM: %s; Name: %s; Beschreibung: %s; ErstelltAm: %s; Status: %s" % (vm_name, snapshot.name, snapshot.description,snapshot.createTime, snapshot.state)
+        snap_text = "VM: %s; SnapshotName: %s; Description: %s; CreatedAt: %s; Status: %s" % (vm_name, snapshot.name, snapshot.description,snapshot.createTime, snapshot.state)
         snapshot_data.append(snap_text)
         snapshot_data = snapshot_data + list_snapshots_recursively(vm_name, snapshot.childSnapshotList)
     return snapshot_data
 
-# Main Funktion
+# Main function
 def main():
-    # Versuche Verbindung zur API aufzubauen...
-    debug_print("Verindungaufbau zu Server \"%s\" mit Benutzer \"%s\"" % (config['hostname'], config['username']))
+    # Try to establish connection to API
+    debug_print("Trying to establish connection to server \"%s\" using account \"%s\"" % (config['hostname'], config['username']))
     si = connect.Connect(config['hostname'], 443, config['username'], config['password'], sslContext=ssl._create_unverified_context())
     atexit.register(Disconnect, si)
     content = si.RetrieveContent()
 
-    # Erstelle rekursiven ContainerView aus der Wurzel-Ebene der alle VMs enthält
+    # Create recursive container view from root level that contains all existing VMs
     container = content.rootFolder
     viewType = [vim.VirtualMachine]
     recursive = True
     containerView = content.viewManager.CreateContainerView(container, viewType, recursive)
 
     children = containerView.view
-    # Über VMs iterieren um nach Snapshots zu suchen
+    # Iterate over VMs to look for snapshots
     for child in children:
         vm_name = child.summary.config.name
 
-        # Lade VM-Objekt
+        # Create VM object for current (index) VM
         vm = get_obj(content, [vim.VirtualMachine], vm_name)
 
-        # Wenn keine Snapshots, for-loop brechen
+        # Continue for loop in case there are no snapshots
         if vm.snapshot is None:
-            debug_print("VM \"%s\" besitzt aktuell keine Snapshots" % vm.name)
+            debug_print("VM \"%s\" doesn't have any" % vm.name)
             continue
 
-        # Maschine hat snapshots => alle anzeigen
+        # List snapshots
         snapshot_paths = list_snapshots_recursively(vm_name, vm.snapshot.rootSnapshotList)
-        debug_print("VM \"%s\" besitzt aktuell %d Snapshot:" % (vm.name, len(snapshot_paths)))
+        debug_print("VM \"%s\" has %d snapshots:" % (vm.name, len(snapshot_paths)))
         for snapshot in snapshot_paths:
             print(snapshot)
 
